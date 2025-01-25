a67195d51ac2be78fe7d4d29bee2fc129d2196b5
BISMAGHANI <hammadworks123@gmail.com>
2025-01-25 11:00:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const {createClient} =supabase
 // console.log(supabase)
-const supabaseUrl="https://wjtoscuovdylskuflwpo.supabase.co"
-const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdG9zY3VvdmR5bHNrdWZsd3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1MjMyODQsImV4cCI6MjA1MzA5OTI4NH0.W9pTlvZRD3qfqZBAbgY8p7KTZw-zvf_V9ooIRL2T1Rc"
+const supabaseUrl="https://csfigvjdpiwrkojsfsnh.supabase.co"
+supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZmlndmpkcGl3cmtvanNmc25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NjkyMDksImV4cCI6MjA1MzI0NTIwOX0.csTGlVo_IB1n_6MKj9lG1HmIeBxuyV1oTPVnVsY7UK4"
 
 const supabaseClient = createClient(supabaseUrl, supabaseKey)
 
