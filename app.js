5b04073e79225e3e2c6491fe368da77648355a3a
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-01-31 20:18:21 +0500

    Added Filter By Price Logic

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let formBtn = document.getElementById("form-btn")
 let form = document.getElementById("form")
 let formCloseBtn = document.getElementById("form-close-btn")
 let loaderScreen = document.getElementById("loader-screen")
+let filterSelect = document.getElementById('filterSelect')
+let allBooks = []
 async function getData() {
     try {
         const { data, error } = await supabase
@@ -10,22 +12,44 @@ async function getData() {
             .select()
         loaderScreen.classList.add("show")
         if (error) throw error
-        console.log(data);
         if (data) {
+            allBooks = data;
+            displayBooks(allBooks);
             loaderScreen.classList.remove("hide")
 
-                ; for (let val of data) {
-                    booksGrid.innerHTML += `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${val.image_url}" alt="${val.title}"></div>
-        <div class="col-6 text-start"><h5 class="">${val.title}</h5><h6 class="">Author:${val.author}</h6><p class=""><b> Description </b><br> ${val.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs 4560</span> <button class="btn btn-dark">Buy Now</button></div></div>
-        <div></div></div></div>`
-                }
+                ;
         }
     } catch (error) {
         console.log(error);
 
     }
-
 }
+function displayBooks(books) {
+    console.log(books);
+    booksGrid.innerHTML = books.map((book) => {
+        return `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${book.image_url}" alt="${book.title}"></div>
+        <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span> <button class="btn btn-dark">Buy Now</button></div></div>
+        <div></div></div></div>`
+    }).join('')
+}
+filterSelect.addEventListener('change', function () {
+    let selectedValue = filterSelect.value;
+    let filteredBooks = [];
+
+    if (selectedValue === "all") {
+        filteredBooks = allBooks;
+    } else if (selectedValue === "0-500") {
+        filteredBooks = allBooks.filter(book => book.price >= 0 && book.price <= 500);
+    } else if (selectedValue === "501-1000") {
+        filteredBooks = allBooks.filter(book => book.price >= 501 && book.price <= 1000);
+    } else if (selectedValue === "1001-2000") {
+        filteredBooks = allBooks.filter(book => book.price >= 1001 && book.price <= 2000);
+    } else if (selectedValue === "2001-5000") {
+        filteredBooks = allBooks.filter(book => book.price >= 2001 && book.price <= 5000);
+    }
+
+    displayBooks(filteredBooks);
+});
 if (formBtn) {
     formBtn.addEventListener('click', show)
 }
@@ -33,7 +57,7 @@ function show() {
     form.classList.remove("show")
     form.classList.add("hide")
 }
-if(formCloseBtn){
+if (formCloseBtn) {
     formCloseBtn.addEventListener('click', close)
 }
 function close() {
