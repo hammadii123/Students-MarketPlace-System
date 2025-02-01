04a56b1fefd20aa41ed3ec8433429c2d5da75cf0
Hammad2 <hammadworks123@gmail.com>
2025-02-01 18:09:58 +0500

    styled cards

--- app.js
+++ app.js
@@ -29,11 +29,26 @@ async function getData() {
 }
 function displayBooks(books) {
     booksGrid.innerHTML = books.map((book) => {
-        return `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${book.image_url}" alt="${book.title}"></div>
-        <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span>
-        <button class="btn btn-dark" onclick="addFavBook(${book.id})">Add to favourite</button></div></div>
-        <div></div></div></div>`
-    }).join('')
+        return `
+        <div class="col p-3 p-lg-4">
+            <div class="card-book h-100 style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
+                <img src="${book.image_url}" class="card-img" alt="${book.title}">
+                <div class="card-body">
+                    <h5 class="book-title">${book.title}</h5>
+                    <p class="book-author">
+                        <i class="bi bi-person-fill me-2"></i>${book.author}
+                    </p>
+                    <p class="book-description">${book.description}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span class="price-tag">₹${book.price}</span>
+                        <button class="btn btn-buy">
+                            <i class="bi bi-cart3 me-2"></i>Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    }).join('');
 }
 if (AllBooksBtn) {
     AllBooksBtn.addEventListener('click', displayBooks)
