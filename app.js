1b30e03d2fef8fa8a6d2090a74336987e35eb653
Hammad2 <hammadworks123@gmail.com>
2025-02-01 19:01:29 +0500

    updated card

--- app.js
+++ app.js
@@ -29,26 +29,27 @@ async function getData() {
 }
 function displayBooks(books) {
     booksGrid.innerHTML = books.map((book) => {
-        return `
-        <div class="col p-3 p-lg-4">
-            <div class="card-book h-100 style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
-                <img src="${book.image_url}" class="card-img" alt="${book.title}">
-                <div class="card-body">
-                    <h5 class="book-title">${book.title}</h5>
-                    <p class="book-author">
-                        <i class="bi bi-person-fill me-2"></i>${book.author}
-                    </p>
-                    <p class="book-description">${book.description}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <span class="price-tag">₹${book.price}</span>
-                        <button class="btn btn-buy">
-                            <i class="bi bi-cart3 me-2"></i>Buy Now
-                        </button>
-                    </div>
+    
+    return `
+    <div class="col p-3 p-lg-4">
+        <div class="card-book h-100 style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
+            <img src="${book.image_url}" class="card-img" alt="${book.title}">
+            <div class="card-body">
+                <h5 class="book-title">${book.title}</h5>
+                <p class="book-author">
+                    <i class="bi bi-person-fill me-2"></i>${book.author}
+                </p>
+                <p class="book-description">${book.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <span class="price-tag">₹${book.price}</span>
+                    <button class="btn btn-buy" onclick="addFavBook(${book.id})">
+                        <i class="bi bi-cart3 me-2"></i>Add to favourite
+                    </button>
                 </div>
             </div>
-        </div>`;
-    }).join('');
+        </div>
+    </div>`;
+}).join('');
 }
 if (AllBooksBtn) {
     AllBooksBtn.addEventListener('click', displayBooks)
