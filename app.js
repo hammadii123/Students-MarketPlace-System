fc21c786f34be1a2688637bab37bdee298029871
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-02-01 15:20:26 +0500

    Added Filter And Favourite

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let booksGrid = document.getElementById("books-grid")
+let favBookGrid = document.getElementById('Fav-books-grid')
 let formBtn = document.getElementById("form-btn")
 let form = document.getElementById("form")
 let formCloseBtn = document.getElementById("form-close-btn")
 let loaderScreen = document.getElementById("loader-screen")
 let filterSelect = document.getElementById('filterSelect')
+let AllBooksBtn = document.getElementById('AllBooks')
+let FavouritesBtn = document.getElementById('Favourites')
 let allBooks = []
 async function getData() {
     try {
@@ -25,12 +28,15 @@ async function getData() {
     }
 }
 function displayBooks(books) {
-    console.log(books);
     booksGrid.innerHTML = books.map((book) => {
         return `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${book.image_url}" alt="${book.title}"></div>
-        <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span> <button class="btn btn-dark">Buy Now</button></div></div>
+        <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span>
+        <button class="btn btn-dark" onclick="addFavBook(${book.id})">Add to favourite</button></div></div>
         <div></div></div></div>`
     }).join('')
+}
+if (AllBooksBtn) {
+    AllBooksBtn.addEventListener('click', displayBooks)
 }
 filterSelect.addEventListener('change', function () {
     let selectedValue = filterSelect.value;
@@ -50,6 +56,180 @@ filterSelect.addEventListener('change', function () {
 
     displayBooks(filteredBooks);
 });
+window.addFavBook = addFavBook
+async function addFavBook(id) {
+    console.log(id);
+
+    let currentUser = localStorage.getItem('currentUser')
+    currentUser = JSON.parse(currentUser)
+    try {
+        const { data, error } = await supabase
+            .from('favBooks')
+            .select('id')
+            .eq('userId', currentUser.userId)
+            .eq('bookId', id);
+        if (error) throw error
+        if (data.length > 0) {
+            await supabase
+                .from('favBooks')
+                .delete()
+                .match({ userId: currentUser.userId, bookId: id });
+
+                Swal.fire({
+                    title: "Info",
+                    text: "Removed From Favourites",
+                    icon: "error"
+                  });
+        }
+        else {
+            try {
+                const { error: storeError } = await supabase
+                    .from('favBooks')
+                    .insert({ userId: currentUser.userId, bookId: id, email: currentUser.email })
+                if (storeError) throw storeError
+
+                Swal.fire({
+                    title: "Info",
+                    text: "Added to Favourites",
+                    icon: "success"
+                  });
+
+            } catch (error) {
+                console.log(error);
+
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+async function getFavBook() {
+
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'))
+    let userId = currentUser.userId
+    try {
+        const { data, error } = await supabase
+            .from('favBooks')
+            .select('bookId')
+            .eq('userId', userId);
+        if (error) throw error
+        if (data) {
+            console.log(data);
+
+            let favouriteIds = data.map((fav) => fav.bookId)
+            console.log(favouriteIds);
+            if (favouriteIds.length > 0) {
+                try {
+                    const { data: favData, error: favError } = await supabase
+                        .from('Books')
+                        .select('*')
+                        .in('id', favouriteIds);
+                    if (favError) throw favError
+                    if (favData) {
+                        console.log(favData);
+                        showFavBooks(favData)
+                    }
+                } catch (error) {
+                    console.log(error);
+
+                }
+            }
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function showFavBooks(Data) {
+    console.log('running...');
+
+    if (Data.length === 0) {
+        favBookGrid.innerHTML = "<p>No favorite books found.</p>";
+        booksGrid.style.display = "none";
+        favBookGrid.style.display = "flex";
+    }
+    favBookGrid.innerHTML = Data.map((book) => {
+        return `  
+        <div class="col">
+            <div class="row align-items-center shadow bg-body-tertiary border rounded-4">
+                <div class="col-6">
+                    <img class="w-100" src="${book.image_url}" alt="${book.title}">
+                </div>
+                <div class="col-6 text-start">
+                    <h5>${book.title}</h5>
+                    <h6>Author: ${book.author}</h6>
+                    <p><b>Description</b><br> ${book.description}</p>
+                    <div class="d-flex justify-content-between align-items-center my-2">
+                        <span>Rs ${book.price}</span>
+                        <button class="btn btn-dark">Buy Now</button>
+                        <button class="btn btn-outline-danger" onclick="removeFav(${book.id})">❌</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    }).join('');
+}
+window.removeFav = removeFav
+async function removeFav(id) {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'))
+    let userId = currentUser.userId
+    try {const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+      });
+      swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+            await supabase
+            .from('favBooks')
+            .delete()
+            .match({ userId: userId, bookId: id });
+            getFavBook()
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          
+        }
+      });
+       
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+if (AllBooksBtn) {
+    AllBooksBtn.addEventListener('click', function () {
+        booksGrid.style.display = "flex"; 
+        favBookGrid.style.display = "none"; 
+        displayBooks(allBooks);
+    });
+}
+
+if (FavouritesBtn) {
+    FavouritesBtn.addEventListener('click', function () {
+        booksGrid.style.display = "none"; 
+        favBookGrid.style.display = "flex";  
+        getFavBook();
+    });
+}
 if (formBtn) {
     formBtn.addEventListener('click', show)
 }
--- auth.js
+++ auth.js
@@ -3,13 +3,13 @@ let signup_Name = document.getElementById("signup_name");
 let signup_Password = document.getElementById("signup_password");
 let signup_Button = document.getElementById("signup_button");
 let signup_spinner = document.getElementById("spinner");
+let logOutBtn = document.getElementById('logOut')
+let signUp_with_google_btn = document.getElementById("google-signup")
 
-let signUp_with_google_btn= document.getElementById("google-signup")
 
-
-let signin_Email=document.getElementById("signin_email");
-let signin_Password=document.getElementById("signin_password");
-let signin_Button=document.getElementById("signin_button");
+let signin_Email = document.getElementById("signin_email");
+let signin_Password = document.getElementById("signin_password");
+let signin_Button = document.getElementById("signin_button");
 
 // const Swal = require('sweetalert2')
 
@@ -34,12 +34,12 @@ async function signup() {
         icon: "success",
         draggable: true,
       });
-
-       //  Set role based on email
-       const role = signup_Email.value === "hammad@mailinator.com" ? "admin" : "user";
+window.location.href = 'login.html'
+      //  Set role based on email
+      const role = signup_Email.value === "hammad@mailinator.com" ? "admin" : "user";
 
       //  Insert the user into the "users" table
-      
+
       const { error: userError } = await supabase
 
         .from("users")
@@ -77,7 +77,7 @@ async function signup() {
     signup_spinner.style.display = "none"; // Hide spinner
   }
 }
-async function signUpGoogle(){
+async function signUpGoogle() {
   try {
     event.preventDefault()
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -88,81 +88,81 @@ async function signUpGoogle(){
     })
 
     if (error) throw error;
-    if (data){
+    if (data) {
       console.log(data);
-   
-    
 
-} 
+
+
+    }
   }
 
-catch (error){
-  console.log("Error:"+error);
-}
+  catch (error) {
+    console.log("Error:" + error);
+  }
 }
 
 
 
 async function signIn() {
-    try {   
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: signin_Email.value,
-            password: signin_Password.value,
-          })
-          if (error) throw error;
-          console.log(data);
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: signin_Email.value,
+      password: signin_Password.value,
+    })
+    if (error) throw error;
+    console.log(data);
 
-          if(data){
-            Swal.fire({
-                title: "Login successful",
-                icon: "success",
-                draggable: true
-              });
+    if (data) {
+      Swal.fire({
+        title: "Login successful",
+        icon: "success",
+        draggable: true
+      });
 
-              console.log(data.user.email)
+      console.log(data.user.email)
 
-              // if (data.user.email  =="hammad@mailinator.com" && data.user.id=="95ae4d45-881a-48fe-b1eb-0e31283efbed"){
-              //   // console.log(email.value,password.value);
+      // if (data.user.email  =="hammad@mailinator.com" && data.user.id=="95ae4d45-881a-48fe-b1eb-0e31283efbed"){
+      //   // console.log(email.value,password.value);
 
-              //   window.location.href = '/dashboard.html'
-              // }
-               // Fetch user's role from the "users" table
+      //   window.location.href = '/dashboard.html'
+      // }
+      // Fetch user's role from the "users" table
 
 
 
-  // This is my auth logic for the future 
-    //   const { data: userData, error: roleError } = await supabase
-    //   .from("users")
-    //   .select("role")
-    //   .eq("id", data.user.id)
-    //   .single();
+      // This is my auth logic for the future 
+      //   const { data: userData, error: roleError } = await supabase
+      //   .from("users")
+      //   .select("role")
+      //   .eq("id", data.user.id)
+      //   .single();
 
-    // if (roleError) throw roleError; 
+      // if (roleError) throw roleError; 
 
-    // console.log("User Role:", userData.role);
+      // console.log("User Role:", userData.role);
 
-    // // Redirect based on role
-    // if (userData.role === "admin") {
-    //   window.location.href = "/dashboard.html"; // Admin goes to dashboard
-    // } else {
-    //   window.location.href = "/marketplace.html"; // User goes to marketplace
-    // }
-    
-    
-      window.location.href = "/dashboard.html"; // User goes to marketplace
-          }
+      // // Redirect based on role
+      // if (userData.role === "admin") {
+      //   window.location.href = "/dashboard.html"; // Admin goes to dashboard
+      // } else {
+      //   window.location.href = "/marketplace.html"; // User goes to marketplace
+      // }
 
-          return data
+
+      window.location.href = "/marketplace.html"; // User goes to marketplace
     }
-    catch(error){
-        console.log(error)
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Invalid email or password!",
-    
-          });
-    }
+
+    return data
+  }
+  catch (error) {
+    console.log(error)
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Invalid email or password!",
+
+    });
+  }
 
 }
 
@@ -175,37 +175,70 @@ if (signup_Button) {
 }
 
 
-if(signUp_with_google_btn){
+if (signUp_with_google_btn) {
   signUp_with_google_btn.addEventListener("click", signUpGoogle);
 }
 
 
 // if signin button is clicked
-if(signin_Button){
-    signin_Button.addEventListener("click",signIn)
+if (signin_Button) {
+  signin_Button.addEventListener("click", signIn)
 }
 
 async function checkSession() {
   try {
-      const { data, error } = await supabase.auth.getSession();
-      if (error) throw error;
+    const { data, error } = await supabase.auth.getSession();
+    if (error) throw error;
 
-      let authPages = ["/index.html", "/login.html", "/"];
-      let currentPath = window.location.pathname;
-      let isAuth = authPages.some((page) => page.includes(currentPath));
-      const session = data.session;
-      if (session) {
-          if (isAuth) {
-              window.location.href = "/marketplace.html";
-          }
+    let authPages = ["/index.html", "/login.html", "/"];
+    let currentPath = window.location.pathname;
+    let isAuth = authPages.some((page) => page.includes(currentPath));
+    const session = data.session;
+    if (session) {
+      if (isAuth) {
+        window.location.href = "/marketplace.html";
       }
-      if (session === null) {
-          if (!isAuth) {
-              window.location.href = "/login.html";
-          }
+    }
+    if (session === null) {
+      if (!isAuth) {
+        window.location.href = "/login.html";
       }
+    }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
 checkSession();
+
+async function currentUser() {
+  try {
+    const { data: { user }, error } = await supabase.auth.getUser()
+    if (error) throw error
+    if (user) {
+      let currentUser = {
+        userId: user.id,
+        email: user.email
+      }
+      localStorage.setItem('currentUser', JSON.stringify(currentUser))
+    }
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+currentUser()
+
+async function logOut(params) {
+  try {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+    window.location.reload()
+    localStorage.removeItem('currentUser')
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logOut)
+}
