f1dde122631847629053352aae06ad4d44f2fe8a
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-02-08 10:44:54 +0500

    Update in app.js
    Favourite card fix

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ async function getData() {
     }
 }
 function displayBooks(books) {
+    favBookGrid.style.display = "none";
     booksGrid.innerHTML = books.map((book) => {
         return `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${book.image_url}" alt="${book.title}"></div>
         <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span>
@@ -151,25 +152,25 @@ async function showFavBooks(Data) {
         favBookGrid.style.display = "flex";
     }
     favBookGrid.innerHTML = Data.map((book) => {
-        return `  
-        <div class="col">
-            <div class="row align-items-center shadow bg-body-tertiary border rounded-4">
-                <div class="col-6">
-                    <img class="w-100" src="${book.image_url}" alt="${book.title}">
-                </div>
-                <div class="col-6 text-start">
-                    <h5>${book.title}</h5>
-                    <h6>Author: ${book.author}</h6>
-                    <p><b>Description</b><br> ${book.description}</p>
-                    <div class="d-flex justify-content-between align-items-center my-2">
-                        <span>Rs ${book.price}</span>
-                        <button class="btn btn-dark">Buy Now</button>
-                        <button class="btn btn-outline-danger" onclick="removeFav(${book.id})">❌</button>
-                    </div>
+        return`
+    <div class="col p-3 p-lg-4">
+        <div class="card-book style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
+            <img src="${book.image_url}" class="card-img" alt="${book.title}">
+            <div class="card-body">
+                <h5 class="book-title">${book.title}</h5>
+                <p class="book-author">
+                    <i class="bi bi-person-fill me-2"></i>${book.author}
+                </p>
+                <p class="book-description">${book.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <span class="price-tag">₹${book.price}</span>
+                    <button class="btn btn-outline-danger" onclick="removeFav(${book.id})">❌</button>
                 </div>
             </div>
-        </div>`;
-    }).join('');
+        </div>
+    </div>`;
+    }).join(''); 
+   
 }
 window.removeFav = removeFav
 async function removeFav(id) {
