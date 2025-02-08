b9ab6d81d168fe90aaa155ce12ab2dc49c8e3eae
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-02-08 10:56:11 +0500

    Update in app.js

--- app.js
+++ app.js
@@ -30,10 +30,25 @@ async function getData() {
 function displayBooks(books) {
     favBookGrid.style.display = "none";
     booksGrid.innerHTML = books.map((book) => {
-        return `  <div class="col"><div class="row align-items-center  shadow bg-body-tertiary border rounded-4"><div class="col-6"><img class="w-100" src="${book.image_url}" alt="${book.title}"></div>
-        <div class="col-6 text-start"><h5 class="">${book.title}</h5><h6 class="">Author:${book.author}</h6><p class=""><b> Description </b><br> ${book.description}</p><div class="d-flex justify-content-between align-items-center my-2"> <span>Rs ${book.price}</span>
-        <button class="btn btn-dark" onclick="addFavBook(${book.id})">Add to favourite</button></div></div>
-        <div></div></div></div>`
+        return `
+    <div class="col p-3 p-lg-4">
+        <div class="card-book h-100 style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
+            <img src="${book.image_url}" class="card-img" alt="${book.title}">
+            <div class="card-body">
+                <h5 class="book-title">${book.title}</h5>
+                <p class="book-author">
+                    <i class="bi bi-person-fill me-2"></i>${book.author}
+                </p>
+                <p class="book-description">${book.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <span class="price-tag">₹${book.price}</span>
+                    <button class="btn btn-buy" onclick="addFavBook(${book.id})">
+                        <i class="bi bi-cart3 me-2"></i>Add to favourite
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>`
     }).join('')
 }
 if (AllBooksBtn) {
@@ -76,11 +91,11 @@ async function addFavBook(id) {
                 .delete()
                 .match({ userId: currentUser.userId, bookId: id });
 
-                Swal.fire({
-                    title: "Info",
-                    text: "Removed From Favourites",
-                    icon: "error"
-                  });
+            Swal.fire({
+                title: "Info",
+                text: "Removed From Favourites",
+                icon: "error"
+            });
         }
         else {
             try {
@@ -93,7 +108,7 @@ async function addFavBook(id) {
                     title: "Info",
                     text: "Added to Favourites",
                     icon: "success"
-                  });
+                });
 
             } catch (error) {
                 console.log(error);
@@ -152,7 +167,7 @@ async function showFavBooks(Data) {
         favBookGrid.style.display = "flex";
     }
     favBookGrid.innerHTML = Data.map((book) => {
-        return`
+        return `
     <div class="col p-3 p-lg-4">
         <div class="card-book style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
             <img src="${book.image_url}" class="card-img" alt="${book.title}">
@@ -169,48 +184,49 @@ async function showFavBooks(Data) {
             </div>
         </div>
     </div>`;
-    }).join(''); 
-   
+    }).join('');
+
 }
 window.removeFav = removeFav
 async function removeFav(id) {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'))
     let userId = currentUser.userId
-    try {const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: "btn btn-success",
-          cancelButton: "btn btn-danger"
-        },
-        buttonsStyling: false
-      });
-      swalWithBootstrapButtons.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Yes, delete it!",
-        cancelButtonText: "No, cancel!",
-        reverseButtons: true
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-            await supabase
-            .from('favBooks')
-            .delete()
-            .match({ userId: userId, bookId: id });
-            getFavBook()
-          swalWithBootstrapButtons.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success"
-          });
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          
-        }
-      });
-       
+    try {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await supabase
+                    .from('favBooks')
+                    .delete()
+                    .match({ userId: userId, bookId: id });
+                getFavBook()
+                swalWithBootstrapButtons.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+
+            }
+        });
+
     } catch (error) {
         console.log(error);
 
@@ -218,31 +234,17 @@ async function removeFav(id) {
 }
 if (AllBooksBtn) {
     AllBooksBtn.addEventListener('click', function () {
-        booksGrid.style.display = "flex"; 
-        favBookGrid.style.display = "none"; 
+        booksGrid.style.display = "flex";
+        favBookGrid.style.display = "none";
         displayBooks(allBooks);
     });
 }
 
 if (FavouritesBtn) {
     FavouritesBtn.addEventListener('click', function () {
-        booksGrid.style.display = "none"; 
-        favBookGrid.style.display = "flex";  
+        booksGrid.style.display = "none";
+        favBookGrid.style.display = "flex";
         getFavBook();
     });
 }
-if (formBtn) {
-    formBtn.addEventListener('click', show)
-}
-function show() {
-    form.classList.remove("show")
-    form.classList.add("hide")
-}
-if (formCloseBtn) {
-    formCloseBtn.addEventListener('click', close)
-}
-function close() {
-    form.classList.add("show")
-    form.classList.remove("hide")
-}
-window.onload = getData(), close()
+window.onload = getData()
